5c677079032f615dd087d82305001f3ea6ff1272
Israfil <israfil.guseynov.98@gmail.com>
2023-12-06 02:05:13 -0600

    add navigation bar component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import { Route, Routes } from 'react-router-dom';
 
 import HomePage from './routes/home-page/home-page.component';
+import Navigation from './routes/navigation/navigation.component';
+
+const Shop = () => {
+  return <div>Shop Page</div>;
+};
+
+const Contact = () => {
+  return <div>Contact Page</div>;
+};
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<HomePage />} />
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<HomePage />} />
+        <Route path="shop" element={<Shop />} />
+        <Route path="contact" element={<Contact />} />
+      </Route>
     </Routes>
   );
 };
